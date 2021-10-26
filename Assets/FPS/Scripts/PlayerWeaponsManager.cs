77a8d1c077279c63763dde47dfb3a96b8788c23e
C. Steennis <csteennis@hotmail.nl>
2021-10-26 12:51:19 +0200

    skeleton enemies

--- Assets/FPS/Scripts/PlayerWeaponsManager.cs
+++ Assets/FPS/Scripts/PlayerWeaponsManager.cs
@@ -154,7 +154,7 @@ public class PlayerWeaponsManager : MonoBehaviour
         {
             if(Physics.Raycast(weaponCamera.transform.position, weaponCamera.transform.forward, out RaycastHit hit, 1000, -1, QueryTriggerInteraction.Ignore))
             {
-                if(hit.collider.GetComponentInParent<EnemyController>())
+                if(hit.collider.GetComponentInParent<EnemyController>() || hit.collider.GetComponentInParent<SkeletonController>())
                 {
                     isPointingAtEnemy = true;
                 }
