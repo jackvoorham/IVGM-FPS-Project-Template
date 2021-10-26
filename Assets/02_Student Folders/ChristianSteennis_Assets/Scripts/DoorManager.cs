8bd67c46f6bbce9b5fea9488a554916a45da703e
C. Steennis <csteennis@hotmail.nl>
2021-10-26 15:39:18 +0200

    added axe as weapon

---
+++ Assets/02_Student Folders/ChristianSteennis_Assets/Scripts/DoorManager.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Health), typeof(Actor))]
+public class DoorManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct RendererIndexData
+    {
+        public Renderer renderer;
+        public int materialIndex;
+
+        public RendererIndexData(Renderer renderer, int index)
+        {
+            this.renderer = renderer;
+            this.materialIndex = index;
+        }
+    }
+
+    [Header("Flash on hit")]
+    [Tooltip("The material used for the body of the hoverbot")]
+    public Material bodyMaterial;
+    [Tooltip("The gradient representing the color of the flash on hit")]
+    [GradientUsageAttribute(true)]
+    public Gradient onHitBodyGradient;
+    [Tooltip("The duration of the flash on hit")]
+    public float flashOnHitDuration = 0.5f;
+
+    [Header("Sounds")]
+    [Tooltip("Sound played when recieving damages")]
+    public AudioClip damageTick;
+
+    WeaponController doorDestroyer;
+
+    public UnityAction onDamaged;
+    public UnityAction onDie;
+
+    ActorsManager m_ActorsManager;
+    Health m_Health;
+    Actor m_Actor;
+    Collider[] m_SelfColliders;
+    GameFlowManager m_GameFlowManager;
+    bool m_WasDamagedThisFrame;
+    bool dead = false;
+
+    void Start()
+    {
+        m_ActorsManager = FindObjectOfType<ActorsManager>();
+        DebugUtility.HandleErrorIfNullFindObject<ActorsManager, SkeletonController>(m_ActorsManager, this);
+
+        m_Health = GetComponent<Health>();
+        DebugUtility.HandleErrorIfNullGetComponent<Health, SkeletonController>(m_Health, this, gameObject);
+
+        m_Actor = GetComponent<Actor>();
+        DebugUtility.HandleErrorIfNullGetComponent<Actor, SkeletonController>(m_Actor, this, gameObject);
+
+        m_SelfColliders = GetComponentsInChildren<Collider>();
+
+        m_GameFlowManager = FindObjectOfType<GameFlowManager>();
+        DebugUtility.HandleErrorIfNullFindObject<GameFlowManager, SkeletonController>(m_GameFlowManager, this);
+
+        // Subscribe to damage & death actions
+        m_Health.onDie += OnDie;
+        m_Health.onDamaged += OnDamaged;
+    }
+
+    void Update()
+    {
+        m_WasDamagedThisFrame = false;
+    }
+
+    void OnDamaged(float damage, GameObject damageSource)
+    {
+        // test if the damage source is the player
+        if (damageSource && damageSource.GetComponent<PlayerCharacterController>() && damageSource.GetComponent<PlayerWeaponsManager>().GetActiveWeapon() == doorDestroyer)
+        {
+            if (onDamaged != null)
+            {
+                onDamaged.Invoke();
+            }
+
+            // play the damage tick sound
+            if (damageTick && !m_WasDamagedThisFrame)
+                AudioUtility.CreateSFX(damageTick, transform.position, AudioUtility.AudioGroups.DamageTick, 0f);
+
+            m_WasDamagedThisFrame = true;
+        }
+    }
+
+    void OnDie()
+    {   
+        dead = true;
+        if (onDie != null)
+        {
+            onDie.Invoke();
+        }
+
+        // this will call the OnDestroy function
+        Destroy(gameObject, 0f);
+    }
+}
